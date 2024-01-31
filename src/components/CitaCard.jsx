7dee014a110c59583f2e8a07dce8d2129c851ac4
Camila Alani <alanicamila124@gmail.com>
2024-01-31 00:18:01 -0300

    agregue una card para representar las citas agregadas por el usuario, esta tiene el card body con su estructura y el contenido principal que es, el card title con el nombre de la mascota, card subtitle con el nombre del sueño de la mascota, card text con la informacion de la cita del usuario, fecha, hora y sintomas y por ultimo el boton de borrar que llama a la funcion onBorrar del evento click

--- src/components/CitaCard.jsx
+++ src/components/CitaCard.jsx
@@ -1,10 +1,24 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
 
-
-const CitaCard = () => {
+const CitaCard = ({ cita, onBorrar }) => {
   return (
-    <>
-    
-    </>
+    <Card className="mb-3">
+      <Card.Body>
+        <Card.Title>Mascota: {cita.nombreMascota}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">
+          Dueño: {cita.nombreDueno}
+        </Card.Subtitle>
+        <Card.Text>
+          <strong>Fecha:</strong> {cita.fecha} <br />
+          <strong>Hora:</strong> {cita.hora} <br />
+          <strong>Síntomas:</strong> {cita.sintomas}
+        </Card.Text>
+        <Button variant="danger" onClick={onBorrar}>
+          Borrar
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 
