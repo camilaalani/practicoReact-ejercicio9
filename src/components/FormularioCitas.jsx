d6bde08a66c86a5446ca50632aa49f574744fcf8
Camila Alani <alanicamila124@gmail.com>
2024-01-31 00:16:07 -0300

    agregue el formulario en si, dentro de una card que tiene card header, card body y card footer

--- src/components/FormularioCitas.jsx
+++ src/components/FormularioCitas.jsx
@@ -29,9 +29,63 @@ const FormularioCitas = ({ onAgregarCita }) => {
   };
   
   return (
-    <>
-
-    </>
+    <Card>
+    <Card.Header>
+      Llenar el formulario para crear una cita
+    </Card.Header>
+    <Card.Body>
+    <Form>
+    <Form.Group className="mb-3" controlId="formDueño">
+      <Form.Label>Nombre del dueño</Form.Label>
+      <Form.Control
+        type="text"
+        placeholder="Nombre del dueño"
+        value={nombreDueno}
+        onChange={(e) => setNombreDueno(e.target.value)}
+      />
+    </Form.Group>
+    <Form.Group className="mb-3" controlId="formMascota">
+      <Form.Label>Nombre de la mascota</Form.Label>
+      <Form.Control
+        type="text"
+        placeholder="Nombre de la mascota"
+        value={nombreMascota}
+        onChange={(e) => setNombreMascota(e.target.value)}
+      />
+    </Form.Group>
+    <Form.Group className="mb-3" controlId="formFecha">
+      <Form.Label>Fecha</Form.Label>
+      <Form.Control
+        type="date"
+        value={fecha}
+        onChange={(e) => setFecha(e.target.value)}
+      />
+    </Form.Group>
+    <Form.Group className="mb-3" controlId="formHora">
+      <Form.Label>Hora</Form.Label>
+      <Form.Control
+        type="time"
+        value={hora}
+        onChange={(e) => setHora(e.target.value)}
+      />
+    </Form.Group>
+    <Form.Group className="mb-3" controlId="formSintomas">
+      <Form.Label>Síntomas</Form.Label>
+      <Form.Control
+        as="textarea"
+        rows={3}
+        value={sintomas}
+        onChange={(e) => setSintomas(e.target.value)}
+      />
+    </Form.Group>
+  </Form>
+    </Card.Body>
+    <Card.Footer>
+      <Button variant="primary" onClick={handleAgregarCita}>
+        Agregar Cita
+      </Button>
+    </Card.Footer>
+  </Card>
   );
 };
 
