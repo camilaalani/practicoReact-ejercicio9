8355ecef96817b7336535179f00e0e783478840c
Camila Alani <alanicamila124@gmail.com>
2024-01-31 00:17:15 -0300

    agregue una card donde tenemos el titulo, una verificacion de si hay citas agendadas, un mensaje en casi de que no haya citas y en caso de que las haya se renderiza la lista y tambien se itera sobre cada cita y llama a la card del componente CitaCard para cada una

--- src/components/ListaCitas.jsx
+++ src/components/ListaCitas.jsx
@@ -1,10 +1,23 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import CitaCard from './CitaCard';
 
-
-const ListaCitas = () => {
+const ListaCitas = ({ citas, onBorrarCita }) => {
   return (
-    <>
-      
-    </>
+    <Card>
+      <Card.Body>
+        <h2>Lista de Citas</h2>
+        {citas.length === 0 ? (
+          <p className='text-center mt-3'>No hay citas agendadas</p>
+        ) : (
+          <>
+            {citas.map((cita, index) => (
+              <CitaCard key={index} cita={cita} onBorrar={() => onBorrarCita(index)} />
+            ))}
+          </>
+        )}
+      </Card.Body>
+    </Card>
   );
 };
 
